84e993ff9af44a1c22e3ef394e0bf415028b2ceb
Nia Watson <nia.d.watson@gmail.com>
2022-04-14 12:04:02 -0500

    completed

---
+++ js/break_and_continue.js
@@ -0,0 +1,3 @@
+"use strict";
+
+prompt("Enter a number between 1 and 50")
---
+++ js/for_loops.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var showMultiplicationTable = 7;
+for (var i = 0; i <= 10; i++) {
+    var result = showMultiplicationTable * i;
+    console.log(showMultiplicationTable + ' x ' + i + ' = ' + result);
+}
+
+
+// for (var x=20; x<=200; x++) {
+//     if (x === 0) {
+//         console.log(x +  " is even");
+//     }
+//     else if (x % 2 === 0) {
+//         console.log(x + " is even");
+//     }
+//     else {
+//         console.log(x + " is odd");
+//     }
+// }
+
