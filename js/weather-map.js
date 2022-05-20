aaaede8085c142aa61e0a7c660d5ed57675cae77
Nia Watson <nia.d.watson@gmail.com>
2022-05-20 18:06:14 -0500

    added time

--- js/weather-map.js
+++ js/weather-map.js
@@ -6,6 +6,20 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
 }).done(function(data) {
     console.log('The entire response:', data);
     console.log('Diving in - here is current information: ', data.current);
+    $("#current-weather").append("<p>" + data.current.temp + "</p>")
     console.log('A step further - information for tomorrow: ', data.daily[1]);
-});
+    data.daily.forEach(function (dailyForcast, index){
+        if (index < 5 ){
+            console.log(new Date(dailyForcast.dt*1000));
+            console.log(dailyForcast)
+            $("#forcast").append("<p>" + new Date(dailyForcast.dt*1000) + "</p>")
+            $("#forcast").append("<p>" + dailyForcast.temp.max + "</p>")
 
+        }
+
+        let current_datetime = new Date()
+        console.log(current_datetime.toString())
+    })
+    data.daily.forEach(function (dailyHumidity,index)
+    )
+});
