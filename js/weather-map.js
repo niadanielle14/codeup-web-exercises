08bd72234d088ca55bb40eb92f09ec700e61301c
Nia Watson <nia.d.watson@gmail.com>
2022-05-23 11:39:16 -0500

    add curent date display

--- js/weather-map.js
+++ js/weather-map.js
@@ -10,20 +10,23 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
     console.log('A step further - information for tomorrow: ', data.daily[1]);
     data.daily.forEach(function (dailyForcast, index){
         if (index < 5 ){
-            console.log(new Date(dailyForcast.dt*1000));
+            console.log(new Date(dailyForcast.dt*1000)); // Curent Date Display
             console.log(dailyForcast.temp.max)
             console.log(dailyForcast.humidity)
             console.log(dailyForcast.pressure)
             console.log(dailyForcast.wind_speed)
+            console.log(dailyForcast.weather[0].description)
             $("#forcast").append("<p>" + new Date(dailyForcast.dt*1000) + "</p>") // Current Date & Time Display
-            $("#forcast").append("<p> Temperature: " + dailyForcast.temp.max + "</p>") // Max Temp Display
+            $("#forcast").append("<p> Temperature: " + dailyForcast.temp.max + " °F</p>") // Max Temp Display
+            $("#forcast").append("<p> Description: " + dailyForcast.weather[0].description + " </p>") // Description Display
             $("#forcast").append("<p> Humidity: " + dailyForcast.humidity + "% </p>") // Humidity Display
             $("#forcast").append("<p> Pressure: " + dailyForcast.pressure + "</p>") // Pressure Display
             $("#forcast").append("<p> Wind: " + dailyForcast.wind_speed + " mph</p>") // WindSpeed Display
+
         }
 
         let current_datetime = new Date()
-        console.log(current_datetime.toString())
+        console.log(current_datetime.toString()) // Creates Current Date
 
 
     })
