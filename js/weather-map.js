00cf36ace2fb82739a7f14d2d3ef51f41d6d231b
Nia Watson <nia.d.watson@gmail.com>
2022-05-24 09:12:01 -0500

    stopping point

--- js/weather-map.js
+++ js/weather-map.js
@@ -6,7 +6,11 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
 }).done(function(data) {
     console.log('The entire response:', data);
     console.log('Diving in - here is current information: ', data.current); // Current Data
+
     console.log('A step further - information for tomorrow: ', data.daily[1]); // Daily Data
+
+
+
     data.daily.forEach(function (dailyForcast, index){
         if (index < 5 ){
             console.log(new Date(dailyForcast.dt*1000)); // Current Date Display
@@ -15,6 +19,20 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
             console.log(dailyForcast.pressure)
             console.log(dailyForcast.wind_speed)
             console.log(dailyForcast.weather[0].description)
+            console.log(data.current.weather[0].description)
+
+            var html1 = '<div class="col card" >';
+            html1 += "<p>" + new Date(data.current.dt*1000) + "</p>";
+            html1 += "<p><img src='http://openweathermap.org/img/wn/" + dailyForcast.weather[0].icon + "@2x.png'></p>"
+            html1 += "<p> Temperature: " + data.current.temp.max + " °F</p>"
+            html1 += "<p> Description: " + data.current.weather[0].description + " </p>"
+            html1 += "<p> Humidity: " + data.current.humidity + "% </p>"
+            html1 += "<p> Pressure: " + data.current.pressure + "</p>"
+            html1 += "<p> Wind: " + data.current.wind_speed + " mph</p>"
+            html1 += '</div>';
+            $('#current-weather').append(html1);
+
+
 
             var html = '<div class="col card" >';
             html += "<p>" + new Date(dailyForcast.dt*1000) + "</p>";
@@ -27,14 +45,6 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
             html += '</div>';
             $('#forcast').append(html);
 
-            // $("#forcast").append("<div class='daily'>")
-            // $("#forcast").append("<p>" + new Date(dailyForcast.dt*1000) + "</p>") // Current Date & Time Display
-            // $("#forcast").append("<p> Temperature: " + dailyForcast.temp.max + " °F</p>") // Max Temp Display
-            // $("#forcast").append("<p> Description: " + dailyForcast.weather[0].description + " </p>") // Description Display
-            // $("#forcast").append("<p> Humidity: " + dailyForcast.humidity + "% </p>") // Humidity Display
-            // $("#forcast").append("<p> Pressure: " + dailyForcast.pressure + "</p>") // Pressure Display
-            // $("#forcast").append("<p> Wind: " + dailyForcast.wind_speed + " mph</p>") // WindSpeed Display
-            // $("#forcast").append("</div>")
         }
 
         let current_datetime = new Date()
